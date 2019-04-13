beea71226414ac5d4d1d33f62662de0750dfb466
Oleksandr Zaporozhchenko <zaporole@fit.cvut.cz>
2019-04-13 22:09:37 +0200

    Added client, authentication implemented

--- hw1/firstServer.cpp
+++ hw1/firstServer.cpp
@@ -1,15 +1,32 @@
 #include <cstdlib>
 #include <cstdio>
 #include <sys/socket.h> // socket(), bind(), connect(), listen()
+#include <sys/types.h>
+#include <fcntl.h>
 #include <unistd.h> // close(), read(), write()
 #include <netinet/in.h> // struct sockaddr_in
 #include <arpa/inet.h> // htons(), htonl()
 #include <strings.h> // bzero()
-//#include <string.h>
+#include <chrono>
+#include <string.h>
 #include <thread>
 #include <iostream>
 
 using namespace std;
+#define BUFFER_SIZE 1024
+
+int setNonblock(int fd){
+	int flags;
+	#if defined (O_NONBLOCK)
+		if (-1 == (flags = fcntl(fd, F_GETFL, 0)))
+			flags = 0;
+		return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+	#else
+		flags = 1;
+		return ioctl(fd, FIOBIO, &flags);
+	#endif
+}
+
 
 class CRobotClient {
 public:
@@ -17,22 +34,84 @@ public:
 	void authentication();
 	void moveToGoal();
 	void pickUpMessage();
+	int receivePackage(char * buff, int & recvSize, struct timeval * tv);
 
 private:
 	int m_Socket;
+	char m_Buffer[2054];
 };
 
+int CRobotClient::receivePackage(char * buffer, int & recvSize, struct timeval * tv) {
+	fd_set set;
+	while(true){
+		int retval;
+		FD_ZERO(&set);
+		FD_SET(m_Socket, &set);		
+		retval = select(m_Socket + 1, &set, NULL, NULL, tv);
+		if(retval < 0){
+			perror("Chyba v select()");
+			close(m_Socket);
+			return -1;
+		}
+		if(!FD_ISSET(m_Socket, &set)){
+			// Zde je jasne, ze funkce select() skoncila cekani kvuli timeoutu.
+			cout << "Connection timeout!" << endl;
+			close(m_Socket);
+			return 0;
+		}
+		int bytesRead = recv(m_Socket, buffer, BUFFER_SIZE, 0);
+		if(bytesRead <= 0){
+			perror("Chyba pri cteni ze socketu.");
+			close(m_Socket);
+			return -3;
+		}
+		buffer[bytesRead] = '\0';
+		recvSize = bytesRead;	
+		cout << buffer << endl;	
+		break;
+	}
+	return 0;
+}
+
+
 void CRobotClient::authentication(){
-	cout << "Authentication" << endl;
+	cout << "Authentication, socket: " << m_Socket << endl;
+	int recvSize = 0;
+	char sendBuff[256];
+	//receiving the username
+	bzero(&m_Buffer, sizeof(m_Buffer));
+	bzero(&sendBuff, sizeof(sendBuff));
+
+	
+	struct timeval timeout;
+	timeout.tv_sec = 5;
+	timeout.tv_usec = 0;
+			
+	
+	receivePackage(m_Buffer, recvSize, &timeout);
+	cout << recvSize << endl;
+	if (recvSize > 2 && m_Buffer[recvSize-1] == '\b'
+			&& m_Buffer[recvSize-2] == '\a'){
+		int hashCode = 0, messageSize = 0;
+		for (int i = 0; i < recvSize-2; ++i) hashCode += m_Buffer[i];
+		hashCode *= 1000;
+		hashCode %= 65536;
+		messageSize = sprintf(sendBuff, "%d\a\b", hashCode);
+		cout << hashCode << endl;
+		send(m_Socket, sendBuff, messageSize, MSG_NOSIGNAL);
+		
+	}
+	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 }
 
 void CRobotClient::moveToGoal(){
-	cout << "Moving to the goal" << endl;
+	cout << "Moving to the goal, socket: " << m_Socket << endl; 
+	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 }
 
 void CRobotClient::pickUpMessage(){
-	cout << "Picking up a message" << endl;
-
+	cout << "Picking up a message, socket: " << m_Socket << endl;
+	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 }
 
 
@@ -88,10 +167,10 @@ int main (int argc, char ** argv){
 	struct sockaddr_in remoteAddr;
 	socklen_t addrSize;
 
-    while(1){
+
+    while(true){
         int slaveSocket = accept(masterSocket, 
 				(struct sockaddr *) &remoteAddr, &addrSize);
-
 	if (slaveSocket < 0){
 		perror("Unable to accept a slave socket");
 		return -1;
@@ -105,6 +184,7 @@ int main (int argc, char ** argv){
   return 0;
 
 }
+
 
 
 
